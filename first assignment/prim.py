f860e3fbba2157c92a82cfc66684762ffb6da842
dalbiero <dalbiero@t20.torre.math.unipd.it>
2022-03-22 11:43:07 +0100

    Funziona ma bisogna indagare su heapq

--- first assignment/prim.py
+++ first assignment/prim.py
@@ -1,4 +1,5 @@
-from typing import Dict
+import heapq
+from typing import Dict, List
 
 class Vertex:
     def __init__(self, name, key, parent) -> None:
@@ -8,6 +9,9 @@ class Vertex:
 
     def __eq__(self, other: object) -> bool:
         return isinstance(other, self.__class__) and self.name == other.name
+        
+    def __lt__(self, other: object) -> bool:
+        return self.key < other.key
 
 class Edge:
     def __init__(self, u: Vertex, v: Vertex, weight: int) -> None:
@@ -23,7 +27,7 @@ class Edge:
 class Graph:
     def __init__(self) -> None:
         self.V: Dict[str, Vertex] = {}
-        self.E: list[Vertex] = []
+        self.E: Dict[(str, str), Edge] = {}
 
     def _add_vertex(self, new_vertex: Vertex) -> Vertex:
         if new_vertex.name in self.V:
@@ -32,7 +36,23 @@ class Graph:
         return new_vertex
 
     def _add_edge(self, new_edge: Edge) -> None:
-        self.E.append(new_edge)
+        self.E[(new_edge.u.name, new_edge.v.name)] = new_edge
+
+    def get_adjacent(self, vertex: Vertex) -> List[Vertex]:
+        adjacent_vertices = []
+        for edge in self.E.values():
+            if vertex == edge.u:
+                adjacent_vertices.append(edge.v)
+            elif vertex == edge.v:
+                adjacent_vertices.append(edge.u)
+        return adjacent_vertices
+
+    def get_weight(self, u: Vertex, v:Vertex) -> int:
+        if (u.name, v.name) in self.E:
+            return self.E[(u.name, v.name)].weight
+        if (v.name, u.name) in self.E:
+            return self.E[(v.name, u.name)].weight
+        raise Exception("NOT FOUND")
 
     def load_from_file(self, file_path: str) -> None:
         with open(file_path, 'r') as file:
@@ -55,11 +75,26 @@ def Prim(G: Graph, s: Vertex):
         vertex.key = float('+inf')
         vertex.parent = None
     s.key = 0
+    vertex_heap = list(G.V.values())
+    heapq.heapify(vertex_heap)
+    while len(vertex_heap) != 0:
+        u: Vertex = heapq.heappop(vertex_heap)
+        #print(f"Estratto vertice: {u.name}")
+        for v in G.get_adjacent(u):
+            weight = G.get_weight(u,v)
+            if vertex_heap.count(v) == 1 and weight < v.key:
+                v.key = weight
+                v.parent = u
+        heapq.heapify(vertex_heap)
 
+    print("-"*20)
 graph = Graph()
-#graph.load_from_file('mst_dataset/input_random_01_10.txt')
+#graph.load_from_file('mst_dataset/input_random_02_10.txt')
 #graph.load_from_file('mst_dataset/input_random_50_10000.txt')
 graph.load_from_file('mst_dataset/input_random_68_100000.txt')
 
 starting_node = graph.V['1']
 Prim(graph, starting_node)
+
+for x in graph.V.values():
+    print(f"Nodo: {x.name}, parent: {x.parent.name if x.parent else x.name}")

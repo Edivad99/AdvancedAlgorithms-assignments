13c68a029642da9549fc47168d870af7db2776e4
Davide Albiero <davide2910@live.it>
2022-03-24 16:26:12 +0100

    Aggiunto vertices_adjacent
    Aggiunto una barra di progressione, pip install progress

--- first assignment/prim.py
+++ first assignment/prim.py
@@ -1,15 +1,17 @@
 import heapq
 from typing import Dict, List
+from progress.bar import Bar
 
 class Vertex:
-    def __init__(self, name, key, parent) -> None:
+    def __init__(self, name: str, key: int, parent) -> None:
         self.name = name
         self.key = key
         self.parent = parent
+        self.vertices_adjacent: List[Vertex]= []
 
     def __eq__(self, other: object) -> bool:
         return isinstance(other, self.__class__) and self.name == other.name
-        
+
     def __lt__(self, other: object) -> bool:
         return self.key < other.key
 
@@ -38,16 +40,7 @@ class Graph:
     def _add_edge(self, new_edge: Edge) -> None:
         self.E[(new_edge.u.name, new_edge.v.name)] = new_edge
 
-    def get_adjacent(self, vertex: Vertex) -> List[Vertex]:
-        adjacent_vertices = []
-        for edge in self.E.values():
-            if vertex == edge.u:
-                adjacent_vertices.append(edge.v)
-            elif vertex == edge.v:
-                adjacent_vertices.append(edge.u)
-        return adjacent_vertices
-
-    def get_weight(self, u: Vertex, v:Vertex) -> int:
+    def get_weight(self, u: Vertex, v: Vertex) -> int:
         if (u.name, v.name) in self.E:
             return self.E[(u.name, v.name)].weight
         if (v.name, u.name) in self.E:
@@ -64,6 +57,8 @@ class Graph:
 
             u = self._add_vertex(Vertex(v1, float('+inf'), None))
             v = self._add_vertex(Vertex(v2, float('+inf'), None))
+            u.vertices_adjacent.append(v)
+            v.vertices_adjacent.append(u)
             self._add_edge(Edge(u, v, int(w)))
 
         assert int(vertices) == len(self.V), f"The list of vertices has a different size compared to the number read from file ({int(vertices)}!={len(self.V)})"
@@ -71,30 +66,43 @@ class Graph:
 
 def Prim(G: Graph, s: Vertex):
     """G is the graph, s is the starting node"""
-    for key, vertex in G.V.items():
+    for vertex in G.V.values():
         vertex.key = float('+inf')
         vertex.parent = None
     s.key = 0
     vertex_heap = list(G.V.values())
     heapq.heapify(vertex_heap)
+
+    bar = Bar('Processing', max=len(vertex_heap), suffix='%(index)d/%(max)d - ETA: %(eta)ds')
     while len(vertex_heap) != 0:
         u: Vertex = heapq.heappop(vertex_heap)
-        #print(f"Estratto vertice: {u.name}")
-        for v in G.get_adjacent(u):
+        # print(f"Estratto vertice: {u.name}")
+        for v in u.vertices_adjacent:
             weight = G.get_weight(u,v)
             if vertex_heap.count(v) == 1 and weight < v.key:
+                vertex_heap.remove(v)
                 v.key = weight
                 v.parent = u
-        heapq.heapify(vertex_heap)
+                heapq.heappush(vertex_heap, v)
+                #heapq._siftdown(vertex_heap, 0, vertex_heap.index(v))
+        bar.next()
+    bar.finish()
 
-    print("-"*20)
+
 graph = Graph()
-#graph.load_from_file('mst_dataset/input_random_02_10.txt')
+graph.load_from_file('mst_dataset/input_random_02_10.txt')
 #graph.load_from_file('mst_dataset/input_random_50_10000.txt')
-graph.load_from_file('mst_dataset/input_random_68_100000.txt')
+#graph.load_from_file('mst_dataset/input_random_53_20000.txt')
+#graph.load_from_file('mst_dataset/input_random_68_100000.txt')
 
 starting_node = graph.V['1']
+from time import perf_counter_ns
+s = perf_counter_ns()
 Prim(graph, starting_node)
-
+f = perf_counter_ns()
+print(f"Time: {f-s}")
+sum = 0
 for x in graph.V.values():
-    print(f"Nodo: {x.name}, parent: {x.parent.name if x.parent else x.name}")
+    sum += x.key
+    # print(f"Nodo: {x.name}, parent: {x.parent.name if x.parent else x.name}")
+print(f"MSP: {sum}")

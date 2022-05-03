d31e7d5b2f7ea97dc338ebe9c88b4c193450b655
cristianopanighel <cristiano.panighel@studenti.unipd.it>
2022-05-03 10:34:44 +0200

    Update

--- second assignment/c#/SecondAssignment/SecondAssignment/Program.cs
+++ second assignment/c#/SecondAssignment/SecondAssignment/Program.cs
@@ -1,12 +1,13 @@
 ﻿using SecondAssignment;
 
-//const string FOLDER_PATH = @"/Users/davide/Sviluppo/Advanced Algorithm/second assignment/tsp_dataset";
-const string FOLDER_PATH = @"C:\Users\crist\Desktop\Advanced Algorithm\AdvancedAlgorithms-assignments\second assignment\tsp_dataset";
+const string FOLDER_PATH = @"/Users/davide/Sviluppo/Advanced Algorithm/second assignment/tsp_dataset";
+//const string FOLDER_PATH = @"C:\Users\crist\Desktop\Advanced Algorithm\AdvancedAlgorithms-assignments\second assignment\tsp_dataset";
 
 var graph = await Graph.LoadFromFileAsync(Path.Combine(FOLDER_PATH, "burma14.tsp"));
 
 graph.PrintAdjacentMatrix();
 var vertices = Algorithms.ClosestInsertion(graph);
+//var vertices = Algorithms.NearestNeighbor(graph);
 var verticesPair = vertices.PairWise();
 
 var sum = verticesPair.Sum(x =>

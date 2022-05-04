287dd922928d6ba333f4680ab0f5920bfa9042f5
Davide Albiero <davide2910@live.it>
2022-05-04 13:41:23 +0200

    Update ListExtensions.cs

--- second assignment/c#/SecondAssignment/SecondAssignment/ListExtensions.cs
+++ second assignment/c#/SecondAssignment/SecondAssignment/ListExtensions.cs
@@ -2,7 +2,7 @@
 
 public static class ListExtensions
 {
-	public static IEnumerable<(T, T)> PairWise<T>(this List<T> list)
+    public static IEnumerable<(T, T)> PairWise<T>(this List<T> list)
     {
         return list.Where((e, i) => i < list.Count - 1).Select((e, i) => (e, list[i + 1]));
     }

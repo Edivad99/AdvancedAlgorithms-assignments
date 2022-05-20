dfd713a49bec73f5a2221e03c16208c44151f7a0
cristianopanighel <cristiano.panighel@studenti.unipd.it>
2022-05-20 15:04:51 +0200

    Update Algorithms.cs
    Correzioni ed ottimizzazioni

--- third assignment/c#/ThirdAssignment/ThirdAssignment/Algorithms.cs
+++ third assignment/c#/ThirdAssignment/ThirdAssignment/Algorithms.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata;
+
 namespace ThirdAssignment;
 
 public static class Algorithms
@@ -5,50 +7,35 @@ public static class Algorithms
     // Fix the seed so we get the same results every time
     private static readonly Random rnd = new Random(234);
 
-    /*private static int FullContraction(KargerGraph graph)
-    {
-        Graph newGraph = (Graph)graph.Clone();
-        while(newGraph.V.Count > 2)
-        {
-            Edge e = newGraph.E.ElementAt(rnd.Next(newGraph.E.Count)).Value.First();
-            newGraph = Graph.ContractEdge(newGraph, e);
-        }
-        return newGraph.E.Select(x => x.Value.Count).Sum();
-    }*/
-
     public static int Karger(KargerGraph graph)
     {
         int min = int.MaxValue;
-        /*  
+        int k = Convert.ToInt32(Math.Pow(Math.Log(graph.Vertices), 2));
         for (int i = 0; i < k; i++)
         {
-            int value = FullContraction(graph);
-            Console.WriteLine($"{i}) FullContraction: {value}");
+            int value = RecursiveContract(graph);
             min = Math.Min(min, value);
-            if (min == 1)
-                break; 
-        }*/
+        }
         return min;
     }
 
     public static int RandomSelect(int[] C)
     {
-        int random = rnd.Next(0, C.Last());
-        int r = C[random];
+        int r = rnd.Next(C.Min(), C.Max());
 
         int start = 0, end = C.Length, mid = 0;
-
+        
         while(start < end)
         {
             mid = (start + end) / 2;
-            if (C[mid - 1] <= r  && r < C[mid])
+            if (C[mid - 1] <= r && r < C[mid])
                 break;
-            else if (C[mid] <= r)
+            if (C[mid] <= r)
                 start = mid + 1;
             else if (C[mid] > r)
                 end = mid;
         }
-
+        Console.WriteLine("--------------------------------------------------------------");
         //Array.BinarySearch(C, start, end, r);
         return mid;
     }
@@ -92,6 +79,26 @@ public static class Algorithms
             graph.ContractEdge(u, v);
         }
         return graph;
+    }
+
+    public static int RecursiveContract(KargerGraph graph)
+    {
+        int n = graph.Vertices;
+        if (n <= 6)
+        {
+            KargerGraph g = Contract(graph, 2);
+            return graph.W[0, 0];
+        }
+
+        int t = Convert.ToInt32(Math.Round((n / (Math.Sqrt(2) + 1)), MidpointRounding.ToPositiveInfinity));
+        int[] w = new int[2];
+        for (int i = 0; i < 2; i++)
+        {
+            KargerGraph g = Contract(graph, t);
+            w[i] = RecursiveContract(g);
+        }
+
+        return w.Min();
     }
 }
 

8cea844f8a91a2f84e439ad45a0d220249497785
Davide Albiero <davide2910@live.it>
2022-05-30 15:09:40 +0200

    Aggiunto exporter stoer

--- third assignment/c#/ThirdAssignment/ThirdAssignment/Export.cs
+++ third assignment/c#/ThirdAssignment/ThirdAssignment/Export.cs
@@ -1,4 +1,5 @@
-﻿using System.Globalization;
+﻿using System.Diagnostics;
+using System.Globalization;
 
 namespace ThirdAssignment;
 
@@ -6,7 +7,7 @@ public static class Export
 {
     private static readonly CultureInfo IT = new("it-it");
 
-    private async static Task<List<string>> TestAlgorithmAsync(string folderPath, Func<KargerGraph, Result> algorithm)
+    public async static Task ExportKargerAsync(string folderPath)
     {
         var csv = new List<string>() { "file;min cut;k repetition;discovery time (ms);discovery iteration;execution time (ms);N execution in 1 sec" };
 
@@ -16,28 +17,53 @@ public static class Export
             int execution = 1;
 
             var graph = await KargerGraph.LoadFromFileAsync(file);
-            var results = algorithm.Invoke(graph);
+            var results = KargerAlgorithm.Execute(graph);
             var time = results.ExecutionTime;
 
-            while(time.Seconds < 1)
+            while (time.Seconds < 1)
             {
                 execution++;
-                var partialResults = algorithm.Invoke(graph);
+                var partialResults = KargerAlgorithm.Execute(graph);
                 time = time.Add(partialResults.ExecutionTime);
             }
 
             time = time.Divide(execution);
             csv.Add($"{fileName};{results.Minimum};{results.KRepetition};{results.DiscoveryTime.TotalMilliseconds.ToString("N", IT)};{results.DiscoveryIteration};{time.TotalMilliseconds.ToString("N", IT)};{execution}");
         }
-        return csv;
-    }
 
-    public static async Task ExportKargerAsync(string folderPath)
-    {
-        var csv = await TestAlgorithmAsync(folderPath, graph => KargerAlgorithm.Execute(graph));
         var raw_csv = string.Join("\n", csv);
         Console.WriteLine(raw_csv);
         await File.WriteAllTextAsync("karger.csv", raw_csv);
+    }
+
+    public async static Task ExportStoerWagnerAsync(string folderPath)
+    {
+        var csv = new List<string>() { "file;min cut;execution time (ms);N execution in 1 sec" };
+
+        foreach (var file in Directory.EnumerateFiles(folderPath))
+        {
+            var fileName = Path.GetFileNameWithoutExtension(file);
+            var graph = await Graph.LoadFromFileAsync(file);
+
+            int execution = 0, minCut;
+            TimeSpan time = new();
+            var stopWatch = new Stopwatch();
+            do
+            {
+                execution++;
+                stopWatch.Restart();
+                minCut = StoerWagnerAlgorithm.Execute(graph);
+                stopWatch.Stop();
+                time = time.Add(stopWatch.Elapsed);
+            } while (time.Seconds < 1);
+            time = time.Divide(execution);
+
+            csv.Add($"{fileName};{minCut};{time.TotalMilliseconds.ToString("N", IT)};{execution}");
+        }
+
+        var raw_csv = string.Join("\n", csv);
+        Console.WriteLine(raw_csv);
+        await File.WriteAllTextAsync("stoer_wagner.csv", raw_csv);
     }
 }
 

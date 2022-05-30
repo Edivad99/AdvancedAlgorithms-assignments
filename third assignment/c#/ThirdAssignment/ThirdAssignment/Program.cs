53216e53b541c965853f8cad16982dc03d948242
Davide Albiero <davide2910@live.it>
2022-05-30 15:11:45 +0200

    Update Program.cs

--- third assignment/c#/ThirdAssignment/ThirdAssignment/Program.cs
+++ third assignment/c#/ThirdAssignment/ThirdAssignment/Program.cs
@@ -3,15 +3,14 @@
 const string FOLDER_PATH = @"/Users/davide/Sviluppo/Advanced Algorithm/third assignment/mincut_dataset";
 //const string FOLDER_PATH = @"C:\Users\crist\Desktop\Advanced Algorithm\AdvancedAlgorithms-assignments\third assignment\mincut_dataset";
 
-var graph = await KargerGraph.LoadFromFileAsync(Path.Combine(FOLDER_PATH, "input_random_01_10.txt"));
+var graph = await KargerGraph.LoadFromFileAsync(Path.Combine(FOLDER_PATH, "input_random_04_10.txt"));
+Console.WriteLine("RISULTATO: " + KargerAlgorithm.Execute(graph));
 
 var graph2 = await Graph.LoadFromFileAsync(Path.Combine(FOLDER_PATH, "test.txt"));
-int res = StoerWagnerAlgorithm.Execute(graph2);
-Console.WriteLine(res);
-
-//Console.WriteLine("RISULTATO: " + KargerAlgorithm.Execute(graph));
+Console.WriteLine(StoerWagnerAlgorithm.Execute(graph2));
 
 //await Export.ExportKargerAsync(FOLDER_PATH);
+//await Export.ExportStoerWagnerAsync(FOLDER_PATH);
 
 Console.WriteLine("END");
 Console.ReadKey();
